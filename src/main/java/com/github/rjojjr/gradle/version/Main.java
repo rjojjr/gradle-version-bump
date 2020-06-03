fc54f2efd8dc48c94456d3593886077078c8e491
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-06-03 16:37:01 -0500

    bump-todo-1 - Add bump major, minor, and patch - RK

--- src/main/java/com/github/rjojjr/gradle/version/Main.java
+++ src/main/java/com/github/rjojjr/gradle/version/Main.java
@@ -6,7 +6,20 @@ public class Main {
         FileEditor editor = new FileEditor();
 
         if(args.length == 0 || args[0].equals("beta")){
-            editor.bumpBetaGroovy(null);
+            System.out.println("Bumping beta version");
+            editor.bumpGroovy(null, 3);
+        }
+        if(args[0].toLowerCase().contains("maj")){
+            System.out.println("Bumping major version");
+            editor.bumpGroovy(null,0);
+        }
+        if(args[0].toLowerCase().contains("min")){
+            System.out.println("Bumping minor version");
+            editor.bumpGroovy(null,1);
+        }
+        if(args[0].toLowerCase().contains("pa")){
+            System.out.println("Bumping patch version");
+            editor.bumpGroovy(null,2);
         }
     }
 
